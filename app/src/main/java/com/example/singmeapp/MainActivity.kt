1d79279338e28929111eb893b594fd6c90e704c8
Ivan Tuzov <vanatuzov.2001.ru@mail.ru>
2022-11-17 13:57:46 +0300

    Added basic fragments

--- app/src/main/java/com/example/singmeapp/MainActivity.kt
+++ app/src/main/java/com/example/singmeapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.singmeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.singmeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.libraryItem -> {supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, LibraryFragment.newInstance()).commit()}
+                R.id.profileItem -> {supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, ProfileFragment.newInstance()).commit()}
+                R.id.homeItem -> {supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, HomeFragment.newInstance()).commit()}
+                R.id.messengerItem -> {supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, MessengerFragment.newInstance()).commit()}
+            }
+            true
+        }
     }
+
+
 }
