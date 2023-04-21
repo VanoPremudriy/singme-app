1447652ab17edae3db59012139e063fc9629f56f
Ivan Tuzov <vanatuzov.2001.ru@mail.ru>
2023-04-21 23:21:49 +0300

    Added sorting in discography all fragment

--- app/src/main/java/com/example/singmeapp/fragments/DiscographyAllFragment.kt
+++ app/src/main/java/com/example/singmeapp/fragments/DiscographyAllFragment.kt
@@ -1,26 +1,29 @@
 package com.example.singmeapp.fragments
 
 import android.os.Bundle
+import android.view.*
+import android.view.View.OnClickListener
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuProvider
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.singmeapp.MainActivity
 import com.example.singmeapp.R
 import com.example.singmeapp.adapters.AlbumAdapter
+import com.example.singmeapp.adapters.SortInAdapter
 import com.example.singmeapp.adapters.TrackAdapter
 import com.example.singmeapp.databinding.FragmentDiscographyAllBinding
 import com.example.singmeapp.items.Album
 import com.example.singmeapp.items.Band
 import com.example.singmeapp.items.Track
 import com.example.singmeapp.viewmodels.DiscographyAllViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
-class DiscographyAllFragment : Fragment() {
+class DiscographyAllFragment : Fragment(), MenuProvider, OnClickListener {
 
     lateinit var fragmentActivity: AppCompatActivity
     lateinit var  binding: FragmentDiscographyAllBinding
@@ -30,12 +33,14 @@ class DiscographyAllFragment : Fragment() {
     lateinit var discographyAllViewModel: DiscographyAllViewModel
     lateinit var band: Band
     lateinit var whatIs: String
+    lateinit var optionsMenu: Menu
+    lateinit var mainActivity: MainActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fragmentActivity =  activity as AppCompatActivity
+        mainActivity = activity as MainActivity
         fragmentActivity.supportActionBar?.show()
-        setHasOptionsMenu(true)
         fragmentActivity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
@@ -69,14 +74,15 @@ class DiscographyAllFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDiscographyAllBinding.inflate(layoutInflater)
-
+        fragmentActivity.addMenuProvider(this, viewLifecycleOwner)
         binding.rcDiscographyAll.layoutManager = LinearLayoutManager(context)
+
+        setButtons()
 
         when(whatIs){
             "tracks" -> {
                 discographyAllViewModel.listTrack.observe(viewLifecycleOwner){
-                    tracksAdapter.trackList.clear()
-                    tracksAdapter.trackList.addAll(it as ArrayList<Track>) /* = java.util.ArrayList<com.example.singmeapp.items.Track> */
+                    tracksAdapter.initList(it)
                     binding.rcDiscographyAll.adapter = tracksAdapter
                 }
 
@@ -88,8 +94,7 @@ class DiscographyAllFragment : Fragment() {
             }
             "albums" -> {
                 discographyAllViewModel.listAlbum.observe(viewLifecycleOwner){
-                    albumsAdapter.albumList.clear()
-                    albumsAdapter.albumList.addAll(it as ArrayList<Album>) /* = java.util.ArrayList<com.example.singmeapp.items.Album> */
+                    albumsAdapter.initList(it)
                     binding.rcDiscographyAll.adapter = albumsAdapter
                 }
 
@@ -101,8 +106,7 @@ class DiscographyAllFragment : Fragment() {
             }
             "singles" -> {
                 discographyAllViewModel.listSingle.observe(viewLifecycleOwner){
-                    singlesAdapter.albumList.clear()
-                    singlesAdapter.albumList.addAll(it as ArrayList<Album>) /* = java.util.ArrayList<com.example.singmeapp.items.Album> */
+                    singlesAdapter.initList(it)
                     binding.rcDiscographyAll.adapter = singlesAdapter
                 }
 
@@ -116,22 +120,134 @@ class DiscographyAllFragment : Fragment() {
         return binding.root
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home){
-            val count: Int? = activity?.supportFragmentManager?.backStackEntryCount
-            if (count == 0) {
-                activity?.onBackPressed()
-            } else {
-                findNavController().popBackStack()
-            }
-        }
-
-        return true
-    }
-
     companion object {
 
         @JvmStatic
         fun newInstance() = DiscographyAllFragment()
+    }
+
+    fun setButtons(){
+        binding.tvSortByInDiscographyAll.setOnClickListener(this@DiscographyAllFragment)
+        mainActivity.binding.tvSortByDefault.setOnClickListener(this@DiscographyAllFragment)
+        mainActivity.binding.tvSortByName.setOnClickListener(this@DiscographyAllFragment)
+        mainActivity.binding.tvSortByDate.setOnClickListener(this@DiscographyAllFragment)
+        mainActivity.binding.tvSortByAlbum.setOnClickListener(this@DiscographyAllFragment)
+        mainActivity.binding.tvSortByAlbum.setOnClickListener(this@DiscographyAllFragment)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        optionsMenu = menu
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            android.R.id.home -> {
+                val count: Int? = activity?.supportFragmentManager?.backStackEntryCount
+                if (count == 0) {
+                    activity?.onBackPressed()
+                } else {
+                    findNavController().popBackStack()
+                }
+                return true
+            }
+
+            R.id.action_search_user -> {
+                val searchView = menuItem.actionView as SearchView
+                searchView.queryHint = getString(R.string.type_here_to_search)
+
+
+                searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        return false
+                    }
+
+                    override fun onQueryTextChange(newText: String?): Boolean {
+                        returnAdapter().sortBySearch(newText ?: "")
+                        initAdapter()
+                        return true
+                    }
+
+                })
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            binding.tvSortByInDiscographyAll.id -> {
+                when (whatIs){
+                    "tracks" -> {
+                        mainActivity.binding.tvSortByBand.visibility = View.VISIBLE
+                        mainActivity.binding.tvSortByAlbum.visibility = View.VISIBLE
+                    }
+                    "albums" -> {
+                        mainActivity.binding.tvSortByBand.visibility = View.VISIBLE
+                        mainActivity.binding.tvSortByAlbum.visibility = View.GONE
+                    }
+                    "singles" -> {
+                        mainActivity.binding.tvSortByBand.visibility = View.VISIBLE
+                        mainActivity.binding.tvSortByAlbum.visibility = View.GONE
+                    }
+                }
+                mainActivity.binding.sortMenu.visibility = View.VISIBLE
+                mainActivity.binding.view15.visibility = View.VISIBLE
+                mainActivity.bottomSheetBehavior2.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+            mainActivity.binding.tvSortByDefault.id -> {
+                returnAdapter().sortByDefault()
+                doWhenSort()
+            }
+            mainActivity.binding.tvSortByName.id -> {
+                returnAdapter().sortByName()
+                doWhenSort()
+            }
+            mainActivity.binding.tvSortByDate.id -> {
+                returnAdapter().sortByDate()
+                doWhenSort()
+            }
+            mainActivity.binding.tvSortByAlbum.id -> {
+                returnAdapter().sortByAlbum()
+                doWhenSort()
+            }
+            mainActivity.binding.tvSortByBand.id -> {
+                returnAdapter().sortByBand()
+                doWhenSort()
+            }
+        }
+    }
+
+    fun doWhenSort(){
+        initAdapter()
+        mainActivity.binding.view15.visibility = View.GONE
+        mainActivity.bottomSheetBehavior2.state = BottomSheetBehavior.STATE_HIDDEN
+    }
+
+    fun initAdapter(){
+        when (whatIs){
+            "tracks" -> binding.rcDiscographyAll.adapter = tracksAdapter
+            "albums" ->  binding.rcDiscographyAll.adapter = albumsAdapter
+            "singles" -> binding.rcDiscographyAll.adapter = singlesAdapter
+        }
+    }
+
+    fun returnAdapter(): SortInAdapter{
+        when (whatIs){
+            "tracks" -> return tracksAdapter
+            "albums" -> return  albumsAdapter
+            "singles" -> return singlesAdapter
+        }
+
+        return tracksAdapter
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        optionsMenu.clear()
+        optionsMenu.close()
     }
 }
